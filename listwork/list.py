5af365ab86ce1d48fc33103b7ffe8be3a70e4681
jaseel <jaseeljas24@gmail.com>
2022-06-06 10:29:12 +0530

    list new

---
+++ listwork/list.py
@@ -0,0 +1,23 @@
+# names=["jaseel",96786,"kjhjg"]
+# for i in range(0,len(names)):
+#     print(names[i])
+
+
+numbers=[1,2,4,6,8,9,3]
+# pos_count=0
+# zero_count=0
+# neg_count=0
+# for num in numbers:
+#     if num<0:
+#         neg_count+=1
+#     if num>0:
+#         pos_count+=1
+#     if num==0:
+#         zero_count+=1
+# print("-ve",neg_count)
+# print("+ve",pos_count)
+# print("zero",zero_count)
+sum=0
+for n in numbers:
+    sum=sum+n
+print(sum)
---
+++ pythonlist/list.py
@@ -0,0 +1,8 @@
+names=["jaseel",76476,"book"]
+for i in range(0,len(names)):
+
+
+
+
+
+
