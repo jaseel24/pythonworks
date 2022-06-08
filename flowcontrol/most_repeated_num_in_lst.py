f3fe84610f55bfe7a062e42fb79d6c4077d46d22
jaseel <jaseeljas24@gmail.com>
2022-06-08 20:36:41 +0530

    another msg

--- flowcontrol/most_repeated_num_in_lst.py
+++ flowcontrol/most_repeated_num_in_lst.py
@@ -1,13 +1,22 @@
-List = [2, 1, 2, 2, 1, 3]
-counter = 0
-greatestoccurednum = 0
-for i in List:
-        curr_frequency = List.count(i)
-        # print(List.count(i))
-        if (curr_frequency > counter):
-            counter = curr_frequency
-            greatestoccurednum=i
-print(greatestoccurednum)
+list = [2, 1, 2, 2, 1, 3]
+# counter = 0
+# greatestoccurednum = 0
+# for i in List:
+#         curr_frequency = List.count(i)
+#         # print(List.count(i))
+#         if (curr_frequency > counter):
+#             counter = curr_frequency
+#             greatestoccurednum=i
+# print(greatestoccurednum)
+dup_list=[]
+# for i in range(0,len(list)):
+#     for j in range((i+1),len(list)):
+#         if list[i]==list[j]:
+#             dup_list.append(list[i])
+# print(dup_list)
+
+
+
 
 
 
---
+++ listwork/sumlist.py
@@ -0,0 +1,20 @@
+lst=[2,4,6,8]
+element=12
+# for i in range(0,len(lst)):
+#     for j in range((i+1),len(lst)):
+#         cur_sum=lst[i]+lst[j]
+#         if cur_sum==sum:
+#             print(f"pairs{lst[i]},{lst[j]}")
+#             break
+low=0
+upp=len(lst)-1
+while low<upp:
+    cur_sum=lst[low]+lst[upp]
+    if cur_sum==element:
+        print(f"pair{lst[low]},{lst[upp]}")
+        break;
+    elif cur_sum<element:
+        low+=1
+    elif cur_sum>element:
+        upp-=1
+
